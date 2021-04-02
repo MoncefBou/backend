dc0bfe39b1aecd6a46eab3099dea50af29a33179
HD M2S <hdm2s@MacBook-Pro-de-HD.local>
2021-04-02 17:17:42 +0200

    ex fini

--- api-countries/callAPI.js
+++ api-countries/callAPI.js
@@ -2,10 +2,8 @@ var request = require('request');
 
 request.get('http://localhost:8000/countries', function (err, res, body) {
     var countriesArray = JSON.parse(body);
-    var reverseArray = [];
-    for ( i = 0; i < countriesArray.length; i++) {
-        reverseArray.unshift(countriesArray[i]);
-    }
+   
+    countriesArray.reverse();
 
-    console.log(reverseArray);
-})
+    console.log(countriesArray);
+});
--- api-countries/index.js
+++ api-countries/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var cors = require('cors')
+app.use(cors())
 
 var arrayOfCountries = ['France', 'Spain', 'Argentina', 'Senegal', 'Tunisia'];
 
