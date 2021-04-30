39e1348b4c1a0a48ced05d98bd6f73f423db2af6
Moncef <moncefboughal@hotmail.fr>
2021-04-30 17:32:47 +0200

    countries

--- api-countries/index.js
+++ api-countries/index.js
@@ -3,8 +3,9 @@ const app = express();
 const cors = require('cors')
 app.use(cors())
 
-const arrayOfCountries = ['USA', 'Morocco', 'Argentina', 'Mali', 'Ukraine'];
-
+// const arrayOfCountries = ['USA', 'Morocco', 'Argentina', 'Mali', 'Ukraine'];
+const arrayOfCountries = [{name:'USA', capital:'Washington'}, {name:'France', capital:'Paris'}, {name:'Morocco', capital:'Rabat'}, 
+{name:'Spain', capital:'Madrid'}, {name:'Senegal', capital:'Dakar'}];
 app.get('/countries', (req, res) => res.send(arrayOfCountries))
 
 // SERVER
