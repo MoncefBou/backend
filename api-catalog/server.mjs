2c8ea2bead9c712e3dd10f40dcc05b4941e283bb
Moncef <moncefboughal@hotmail.fr>
2021-05-04 17:27:40 +0200

    create api movie

---
+++ api-catalog/server.mjs
@@ -0,0 +1,23 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+app.use(cors())
+
+import movies from './dataCatalog.mjs';
+
+
+app.get("/movies", (req, res) => {
+    res.json(movies)
+})
+
+app.get("/movies/:id", (req, res) => {
+    let id = parseInt(req.params.id)
+
+    const theMovie = movies.filter(elem => elem.id === id);
+
+    res.json(theMovie)
+})
+
+// SERVER
+const port = 8000;
+app.listen(port, () => console.log('Server', port))
